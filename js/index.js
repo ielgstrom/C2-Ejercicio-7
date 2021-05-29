1770cf81c036c403c637450fd63f2a6be1226d76
Ignasi <i.elgstrom@gmail.com>
2021-05-29 21:56:51 +0200

    Funcions 1 i 2 corretgides again

--- js/index.js
+++ js/index.js
@@ -1,30 +1,18 @@
-const equiposMayoresEdad = (equip, edadMinima) => {
-  const equipoAsignadosMayoresEdad = equip.filter(
-    ({ asignado: { empleado: edad } }) => edad > edadMinima
+const equiposMayoresEdad = (equipos, edadMinima) => {
+  const equiposMayorDeCiertaEdad = equipos.filter(
+    ({
+      asignado: {
+        empleado: { edad },
+      },
+    }) => edad > edadMinima
   );
-  return equiposMayoresEdad;
+  return equiposMayorDeCiertaEdad;
 };
 
-const equiposProvincia = equipos.filter(
-  (equipo, provincia) => equipo.asignado.provincia === provincia
-);
-
-const provincias = equipos.filter((equipo) => equipo.asignado.provincia);
-
-const puestos = equipos.filter((equipo) => equipo.asignado.empleado.puesto);
-
-const edadMedia = equipos.reduce(
-  (
-    acumulador,
-    {
-      equipo: {
-        asignado: {
-          empleado: { edad },
-        },
-      },
-    }
-  ) => acumulador + edad / equipos.length,
-  0
-);
-
-const trabajadoresTipo = (equipos) => console.log("no");
+const equiposProvincia = (equipos, provinciaGeneral) => {
+  const equiposDeProvincias = equipos.filter(
+    ({ asignado: { provincia } }) =>
+      provinciaGeneral === provincia.toLowerCase()
+  );
+  return equiposDeProvincias;
+};
